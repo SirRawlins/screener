807f8bb0f7e91f76d83cae83fb7a0d0baef88da6
Robert Rawlins <robrawlins@gmail.com>
2018-02-02 08:41:57 +0000

    DRYed up the tests with some shared examples.

--- spec/lib/customer_collection_spec.rb
+++ spec/lib/customer_collection_spec.rb
@@ -2,14 +2,15 @@
 
 require 'spec_helper'
 require 'customer_collection'
+require 'support/shared_examples/customer_file_parser'
 
 RSpec.describe CustomerCollection do
 
-	describe "Instance methods" do
+	describe 'Instance methods' do
 		# Initialize a collection for testing.
 		subject { described_class.new(customer_sample) }
 
-		describe "#nearby" do
+		describe '#nearby' do
             # Mock some example customer hashes like those
             # which might be parsed from the file.
             #
@@ -33,51 +34,13 @@ RSpec.describe CustomerCollection do
 		end
 	end
 
-	describe "Class methods" do
-		describe "#from_file" do
+	describe 'Class methods' do
+		describe '#from_file' do
 			# Get the method result to test.
 			subject { described_class.from_file(path) }
 
-			context 'with a well-formed file' do
-				# Mock the path to the well formed file.
-				let(:path) { '/spec/fixtures/customers.json' }
-
-				# Ensure the returned value looks good.
-				it { is_expected.to be_an(Array) }
-				it { is_expected.to have(32).items }
-			end
-
-			context 'with a well-formed, but spacey file' do
-				# Mock the path to the well formed file.
-				let(:path) { '/spec/fixtures/customers-with-blank-line.json' }
-
-				# Ensure the returned value looks good.
-				# It should skip and continue over the blank lines
-				# and parse all the data.
-				it { is_expected.to be_an(Array) }
-				it { is_expected.to have(32).items }
-			end
-
-			context 'with a malformed file' do
-				# Mock the path to the well formed file.
-				let(:path) { '/spec/fixtures/customers-with-malformation.json' }
-
-				it 'raises a JSON parsing error' do
-					# It'll fail to parse the malformed JSON and inform
-					# the user about this.
-					expect { subject }.to raise_error(JSON::ParserError)
-				end
-			end
-
-			context 'without a path' do
-				# Mock a nil path.
-				let(:path) { nil }
-
-				it 'raises an argument error' do
-					# Check it expects the path argument.
-					expect { subject }.to raise_error(ArgumentError)
-				end
-			end
+			# Generic file parser behaviour.
+			it_behaves_like 'customer_file_parser' 
 		end
 	end
 
--- spec/lib/customer_file_spec.rb
+++ spec/lib/customer_file_spec.rb
@@ -2,53 +2,16 @@
 
 require 'spec_helper'
 require 'customer_file'
+require 'support/shared_examples/customer_file_parser'
 
 RSpec.describe CustomerFile do
 
-  describe '#parse' do
-    # Get the result for testing.
-    subject { described_class.parse(path) }
+    describe '#parse' do
+        # Get the result for testing.
+        subject { described_class.parse(path) }
 
-    context 'with a well-formed file' do
-      # Mock the path to the well formed file.
-      let(:path) { '/spec/fixtures/customers.json' }
-
-      # Ensure the returned value looks good.
-      it { is_expected.to be_an(Array) }
-      it { is_expected.to have(32).items }
+        # Generic file parser behaviour.
+        it_behaves_like 'customer_file_parser'
     end
-
-    context 'with a well-formed, but spacey file' do
-      # Mock the path to the well formed file.
-      let(:path) { '/spec/fixtures/customers-with-blank-line.json' }
-
-      # Ensure the returned value looks good.
-      # It should skip and continue over the blank lines
-      # and parse all the data.
-      it { is_expected.to be_an(Array) }
-      it { is_expected.to have(32).items }
-    end
-
-    context 'with a malformed file' do
-      # Mock the path to the well formed file.
-      let(:path) { '/spec/fixtures/customers-with-malformation.json' }
-
-      it 'raises a JSON parsing error' do
-        # It'll fail to parse the malformed JSON and inform
-        # the user about this.
-        expect { subject }.to raise_error(JSON::ParserError)
-      end
-    end
-
-    context 'without a path' do
-      # Mock a nil path.
-      let(:path) { nil }
-
-      it 'raises an argument error' do
-        # Check it expects the path argument.
-        expect { subject }.to raise_error(ArgumentError)
-      end
-    end
-  end
 
 end
