2f1580dabbaed9960bbe400d1c9197a4aea6321e
Robert Rawlins <robrawlins@gmail.com>
2018-02-02 11:05:35 +0000

    A little more tidying up.

--- spec/lib/customer/store_spec.rb
+++ spec/lib/customer/store_spec.rb
@@ -12,22 +12,24 @@ RSpec.describe Customer::Store do
     end
 
     describe '#write' do
-    	# Mock a temp file to hold the data.
-    	let(:output_path) { 'spec/tmp/customer-export.json' }
+        # Get the result for testing.
+        subject { described_class.write(output_path, customers) }
 
-    	# Read in the customer collection from the fixture
-    	# to use in testing the write.
-    	let(:customers) { described_class.parse('/spec/fixtures/customers.json') }
+        context 'With a valid arguments' do
+        	# Mock a temp file to hold the data.
+        	let(:output_path) { 'spec/tmp/customer-export.json' }
 
-    	# Get the result for testing.
-    	subject { described_class.write(output_path, customers) }
+        	# Read in the customer collection from the fixture
+        	# to use in testing the write.
+        	let(:customers) { described_class.parse('/spec/fixtures/customers.json') }
 
-    	# Get the expected file content from the fixtures.
-		let(:expected_output_content) { File.open(File.expand_path("./#{output_path}")).read }
-    
-		# Compare against the fixture data to see that
-		# the expected file was generated.
-		it { is_expected.to eq(expected_output_content) }
+        	# Get the expected file content from the fixtures.
+    		let(:expected_output_content) { File.open(File.expand_path("./#{output_path}")).read }
+        
+    		# Compare against the fixture data to see that
+    		# the expected file was generated.
+    		it { is_expected.to eq(expected_output_content) }
+        end
     end
 
 end
--- spec/lib/inviter_spec.rb
+++ spec/lib/inviter_spec.rb
@@ -4,25 +4,31 @@ require 'inviter'
 RSpec.describe Inviter do
 
 	describe "#prepare_from_file" do
-		# Mock the input file.
-		# Mock the path to the well formed file.
-		let(:input_path) { '/spec/fixtures/customers.json' }
-		# Mock a path to use for the output file.
-		let(:output_path) { '/spec/tmp/customers-export.json' }
-
-		# Get the expected file content from the fixtures.
-		let(:expected_output_content) { File.open(File.expand_path("./spec/fixtures/invitees.json")).read }
-
 		# Mock the result for testing.
-		subject { described_class.prepare_from_file(input_path, output_path, 100, [53.339428, -6.257664]) }
+		subject do
+			# Call the method with the contextual input
+			# and some generic nearby filter attributes.
+			described_class.prepare_from_file(input_path, '/spec/tmp/customers-export.json', 100, [53.339428, -6.257664])
+		end
 
-		# Compare against the fixture data to see that
-		# the expected file was generated.
-		it { is_expected.to eq(expected_output_content) }
+		context "With a good input file" do
+			# Mock the input file.
+			# Mock the path to the well formed file.
+			let(:input_path) { '/spec/fixtures/customers.json' }
 
-		# Fails with parse error when bad file
-		# is supplied as input_path.
-		it_behaves_like 'customer/store/with_bad_file'
+			# Get the expected file content from the fixtures.
+			let(:expected_output_content) { File.open(File.expand_path("./spec/fixtures/invitees.json")).read }
+
+			# Compare against the fixture data to see that
+			# the expected file was generated.
+			it { is_expected.to eq(expected_output_content) }
+		end
+
+		context "With a bad input file" do
+			# Fails with parse error when bad file
+			# is supplied as input_path.
+			it_behaves_like 'customer/store/with_bad_file'
+		end
 	end
 
 end
