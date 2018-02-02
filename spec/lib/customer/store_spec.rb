05d17153c94c060939896241df2f55b3f977b63e
Robert Rawlins <robrawlins@gmail.com>
2018-02-02 10:36:36 +0000

    Rough test case for the store/write method which was missing.

--- spec/lib/customer/store_spec.rb
+++ spec/lib/customer/store_spec.rb
@@ -13,4 +13,23 @@ RSpec.describe Customer::Store do
         it_behaves_like 'customer_file_parser'
     end
 
+    describe '#write' do
+    	# Mock a temp file to hold the data.
+    	let(:output_path) { 'spec/fixtures/invitees.json' }
+
+    	# Read in the customer collection from the fixture
+    	# to use in testing the write.
+    	let(:customers) { described_class.parse('/spec/fixtures/customers.json') }
+
+    	# Get the result for testing.
+    	subject { described_class.write(output_path, customers) }
+
+    	# Get the expected file content from the fixtures.
+		let(:expected_output_content) { File.open(File.expand_path("./#{output_path}")).read }
+    
+		# Compare against the fixture data to see that
+		# the expected file was generated.
+		it { is_expected.to eq(expected_output_content) }
+    end
+
 end
